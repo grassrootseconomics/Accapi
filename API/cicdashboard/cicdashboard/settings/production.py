6ffed38f536683b8f4e12b1ffce8fec3e7f08ef7
mohaibma <mohaibma@yahoo.com>
2020-04-07 16:05:38 +0100

    updating DockerFile

--- API/cicdashboard/cicdashboard/settings/production.py
+++ API/cicdashboard/cicdashboard/settings/production.py
@@ -1,13 +1,13 @@
 from cicdashboard.settings.common import *
 
-import boto3
-import json
+# import boto3
+# import json
 
-secrets_client = boto3.client('secretsmanager')
-secret_arn = 'arn:aws:secretsmanager:eu-central-1:847108109661:secret:/prod/dbcredentials-OEPfTY'
-auth_token = secrets_client.get_secret_value(SecretId=secret_arn).get('SecretString')
-d = json.loads(auth_token)
-print(d['password'])
+# secrets_client = boto3.client('secretsmanager')
+# secret_arn = 'arn:aws:secretsmanager:eu-central-1:847108109661:secret:/prod/dbcredentials-OEPfTY'
+# auth_token = secrets_client.get_secret_value(SecretId=secret_arn).get('SecretString')
+# d = json.loads(auth_token)
+# print(d['password'])
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -36,14 +36,25 @@ CORS_ORIGIN_ALLOW_ALL = True
 # 'https://iqr3ivy96j.execute-api.eu-central-1.amazonaws.com/prod/graphql/'
 # ]
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'cic_dashboard',
+#         'USER': d['username'],
+#         'PASSWORD': d['password'],
+#         'HOST': d['host'],
+#         'PORT': d['port'],
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cic_dashboard',
-        'USER': d['username'],
-        'PASSWORD': d['password'],
-        'HOST': d['host'],
-        'PORT': d['port'],
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': '5432',
     }
 }
 
