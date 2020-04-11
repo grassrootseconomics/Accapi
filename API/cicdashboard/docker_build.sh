f1a25d7557a07c212f01af26b12ad49a0e836c8c
Vaneet <vaneet.gupta@accenture.com>
2020-04-11 15:15:45 +0530

    Adding Environmental Variables

--- API/cicdashboard/docker_build.sh
+++ API/cicdashboard/docker_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-DB_USER=`aws secretsmanager get-secret-value --secret-id /prod/dbcredentials | jq --raw-output .SecretString | jq -r ."username"`
-DB_PASS=`aws secretsmanager get-secret-value --secret-id /prod/dbcredentials | jq --raw-output .SecretString | jq -r ."password"`
-DB_HOST=`aws secretsmanager get-secret-value --secret-id /prod/dbcredentials | jq --raw-output .SecretString | jq -r ."host"`
+DB_USER=`aws secretsmanager get-secret-value --secret-id $secret-id | jq --raw-output .SecretString | jq -r ."username"`
+DB_PASS=`aws secretsmanager get-secret-value --secret-id $secret-id | jq --raw-output .SecretString | jq -r ."password"`
+DB_HOST=`aws secretsmanager get-secret-value --secret-id $secret-d | jq --raw-output .SecretString | jq -r ."host"`
 
 docker build --no-cache -t cic-dashboard-api-prod --build-arg DB_USER=$DB_USER --build-arg DB_PASS=$DB_PASS  --build-arg DB_HOST=$DB_HOST . 
--- API/cicdashboard/ecs_stop_task.sh
+++ API/cicdashboard/ecs_stop_task.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-OLD_TASK_IDS=$(aws ecs list-tasks --cluster cic-production-dashboard --desired-status RUNNING  | egrep "task/" | sed -E "s/.*task\/(.*)\"/\1/" | sed -z 's/\n/ /g')
+OLD_TASK_IDS=$(aws ecs list-tasks --cluster $cluster_id --desired-status RUNNING  | egrep "task/" | sed -E "s/.*task\/(.*)\"/\1/" | sed -z 's/\n/ /g')
 IFS=', ' read -r -a array <<< "$OLD_TASK_IDS"
 for element in "${array[@]}"
 do
