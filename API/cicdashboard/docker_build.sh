e333b3c54f5967dfbe2b43adaff7b96a46402d6a
Vaneet <vaneet.gupta@accenture.com>
2020-04-13 12:18:37 +0530

    Fixed the aws command for secret manager

--- API/cicdashboard/docker_build.sh
+++ API/cicdashboard/docker_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $secret_id
-DB_USER=`aws secretsmanager get-secret-value --secret_id $secret_id | jq --raw-output .SecretString | jq -r ."username"`
-DB_PASS=`aws secretsmanager get-secret-value --secret_id $secret_id | jq --raw-output .SecretString | jq -r ."password"`
-DB_HOST=`aws secretsmanager get-secret-value --secret_id $secret_id | jq --raw-output .SecretString | jq -r ."host"`
+DB_USER=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."username"`
+DB_PASS=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."password"`
+DB_HOST=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."host"`
 
 docker build --no-cache -t $cluster_id --build-arg DB_USER=$DB_USER --build-arg DB_PASS=$DB_PASS  --build-arg DB_HOST=$DB_HOST . 
