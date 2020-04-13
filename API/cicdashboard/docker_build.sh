5f0796df3afcded590f998131d360a14ee4e092a
Vaneet <vaneet.gupta@accenture.com>
2020-04-13 12:12:52 +0530

    Replace the environment variables

--- API/cicdashboard/docker_build.sh
+++ API/cicdashboard/docker_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-DB_USER=`aws secretsmanager get-secret-value --secret-id $secret-id | jq --raw-output .SecretString | jq -r ."username"`
-DB_PASS=`aws secretsmanager get-secret-value --secret-id $secret-id | jq --raw-output .SecretString | jq -r ."password"`
-DB_HOST=`aws secretsmanager get-secret-value --secret-id $secret-d | jq --raw-output .SecretString | jq -r ."host"`
+echo $secret_id
+DB_USER=`aws secretsmanager get-secret-value --secret_id $secret_id | jq --raw-output .SecretString | jq -r ."username"`
+DB_PASS=`aws secretsmanager get-secret-value --secret_id $secret_id | jq --raw-output .SecretString | jq -r ."password"`
+DB_HOST=`aws secretsmanager get-secret-value --secret_id $secret_id | jq --raw-output .SecretString | jq -r ."host"`
 
 docker build --no-cache -t $cluster_id --build-arg DB_USER=$DB_USER --build-arg DB_PASS=$DB_PASS  --build-arg DB_HOST=$DB_HOST . 
