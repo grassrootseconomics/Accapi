59d2f822601d5f279af76ea25678e96c19233539
mohaibma <mohaibma@yahoo.com>
2020-04-13 14:48:05 +0100

    Update production.py

--- API/cicdashboard/cicdashboard/settings/production.py
+++ API/cicdashboard/cicdashboard/settings/production.py
@@ -1,12 +1,4 @@
 from cicdashboard.settings.common import *
-# import boto3
-# import json
-
-# secrets_client = boto3.client('secretsmanager')
-# secret_arn = 'arn:aws:secretsmanager:eu-central-1:847108109661:secret:/prod/dbcredentials-OEPfTY'
-# auth_token = secrets_client.get_secret_value(SecretId=secret_arn).get('SecretString')
-# d = json.loads(auth_token)
-# print(d['password'])
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -34,18 +26,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 # 'http://cic-dashboard-frontend-webpage-prod.s3-website.eu-central-1.amazonaws.com',
 # 'https://iqr3ivy96j.execute-api.eu-central-1.amazonaws.com/prod/graphql/'
 # ]
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'cic_dashboard',
-#         'USER': d['username'],
-#         'PASSWORD': d['password'],
-#         'HOST': d['host'],
-#         'PORT': d['port'],
-#     }
-# }
-
 
 DATABASES = {
     'default': {
