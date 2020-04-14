8395b5fd457018c12d14554cd1b86b310ab2a84f
Vaneet <vaneet.gupta@accenture.com>
2020-04-14 17:34:49 +0530

    Troubleshooting DB Details

--- API/cicdashboard/docker_build.sh
+++ API/cicdashboard/docker_build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo $secret_id
-DB_USER=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."username"`
-DB_PASS=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."password"`
-DB_HOST=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."host"`
+export DB_USER=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."username"`
+export DB_PASS=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."password"`
+export DB_HOST=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."host"`
 echo $DB_NAME
-echo $DB_USER
+echo  $DB_USER
 echo $DB_PASS
 echo $DB_HOST
+exit 1
 docker build --no-cache -t $cluster_id --build-arg DB_USER=$DB_USER --build-arg DB_PASS=$DB_PASS  --build-arg DB_HOST=$DB_HOST  --build-arg DB_NAME=$DB_NAME . 
