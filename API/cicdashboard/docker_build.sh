7950f6ebf76e73d11834be9f59c32a6ada6362cf
Vaneet <vaneet.gupta@accenture.com>
2020-04-14 17:11:41 +0530

    Fixing DB Details

--- API/cicdashboard/docker_build.sh
+++ API/cicdashboard/docker_build.sh
@@ -3,5 +3,4 @@ echo $secret_id
 DB_USER=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."username"`
 DB_PASS=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."password"`
 DB_HOST=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."host"`
-echo $DB_NAME
-docker build --no-cache -t $cluster_id --build-arg DB_USER=$DB_USER --build-arg DB_PASS=$DB_PASS  --build-arg DB_HOST=$DB_HOST --build-arg DB_NAME=$DB_NAME . 
+docker build --no-cache -t $cluster_id --build-arg DB_USER=$DB_USER --build-arg DB_PASS=$DB_PASS  --build-arg DB_HOST=$DB_HOST  . 
