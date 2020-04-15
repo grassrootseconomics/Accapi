421064bae45df7ccb228d091b0d197895e6c6989
Vaneet <vaneet.gupta@accenture.com>
2020-04-15 09:50:34 +0530

    Cleaning Master

--- API/cicdashboard/ecs_stop_task.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-OLD_TASK_IDS=$(aws ecs list-tasks --cluster $cluster_id --desired-status RUNNING  | egrep "task/" | sed -E "s/.*task\/(.*)\"/\1/" | sed -z 's/\n/ /g')
-IFS=', ' read -r -a array <<< "$OLD_TASK_IDS"
-for element in "${array[@]}"
-do
-    aws ecs stop-task --cluster $cluster_id --task ${element}
-done
