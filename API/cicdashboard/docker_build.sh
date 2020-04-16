bf34ae5199968f90a16d328609b926c29ad0b529
Vaneet <vaneet.gupta@accenture.com>
2020-04-16 13:08:28 +0530

    Fixing the DB Password environment variable

--- API/cicdashboard/docker_build.sh
+++ API/cicdashboard/docker_build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+echo "Secret ID: $secret_id"
 export DB_USER=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."username"`
 export DB_PASS=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."password"`
 export DB_HOST=`aws secretsmanager get-secret-value --secret-id $secret_id | jq --raw-output .SecretString | jq -r ."host"`
+echo $DB_PASS
 export $env
+exit 1
 docker build --no-cache -t $cluster_id --build-arg DB_USER=$DB_USER --build-arg DB_PASS=$DB_PASS  --build-arg DB_HOST=$DB_HOST  --build-arg DB_NAME=$DB_NAME --build-arg ENVIRONMENT=$env . 
