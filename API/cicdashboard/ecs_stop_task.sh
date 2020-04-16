814413e3d7c44ef60ebc4958504b530864ab0c0b
Vaneet <vaneet.gupta@accenture.com>
2020-04-16 13:50:54 +0530

    Troubleshooting ecs task stop script

--- API/cicdashboard/ecs_stop_task.sh
+++ API/cicdashboard/ecs_stop_task.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 OLD_TASK_IDS=$(aws ecs list-tasks --cluster $cluster_id --desired-status RUNNING  | egrep "task/" | sed -E "s/.*task\/(.*)\"/\1/" | sed -z 's/\n/ /g')
 IFS=', ' read -r -a array <<< "$OLD_TASK_IDS"
+echo $cluster_id
+echo $OLD_TASK_IDS
+exit 1
 for element in "${array[@]}"
 do
     aws ecs stop-task --cluster $cluster_id --task ${element}
