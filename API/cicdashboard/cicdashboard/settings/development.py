48d91af5bf692f5ea5aa3f33571f9c148afdb30b
mohaibma <mohaibma@yahoo.com>
2020-04-17 20:35:04 +0100

    Update development.py

--- API/cicdashboard/cicdashboard/settings/development.py
+++ API/cicdashboard/cicdashboard/settings/development.py
@@ -8,8 +8,8 @@ SECRET_KEY = '1$qpv*d%uoex8xe!as6r0#3s@kz1)e&tpb5a@@@)y#dr(5_**t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = [
-'cic-dashboard-frontend-webpage.s3-website.eu-central-1.amazonaws.com',
-'b51ycagx5g.execute-api.eu-central-1.amazonaws.com',
+'http://cic-dashboard-frontend-webpage.s3-website.eu-central-1.amazonaws.com',
+'https://b51ycagx5g.execute-api.eu-central-1.amazonaws.com',
 'testtg-929153678.eu-central-1.elb.amazonaws.com'
 ]
 
