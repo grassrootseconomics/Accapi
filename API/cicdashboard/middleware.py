f471703c121de7f79326bdf682301a52ae42b5e5
mohaibma <mohaibma@yahoo.com>
2020-04-30 11:12:14 +0100

    Delete middleware.py

--- API/cicdashboard/middleware.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.utils.deprecation import MiddlewareMixin
-
-class HealthEndpointMiddleware(MiddlewareMixin):
-	def process_request(self, request):
-		if request.META["PATH_INFO"] == "/health_check":
-			print(request.META["PATH_INFO"])
-			return HttpResponse("OK")
